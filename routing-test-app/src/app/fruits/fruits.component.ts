1e4cd51f47557104afa8359e6eec2981ce88fe0a
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-05-30 12:55:59 +0000

    parte 1

---
+++ routing-test-app/src/app/fruits/fruits.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruits',
+  templateUrl: './fruits.component.html',
+  styleUrls: ['./fruits.component.css']
+})
+export class FruitsComponent {
+  fruits = [
+    { name: '9erd', emoji: '🍇' },
+    { name: 'kelb', emoji: '🍈' },
+    { name: 'dib', emoji: '🍇' },
+    { name: 'mesh', emoji: '🍇' },
+    { name: 'nmer', emoji: '🍇' },
+    { name: 'fa8d', emoji: '🍇' },
+    { name: '3awd', emoji: '🍇' },
+    { name: 'tour', emoji: '🍇' },
+    { name: 'begra', emoji: '🍈' },
+    { name: '7ellouf', emoji: '🍇' },
+    { name: 'khenzir', emoji: '🍇' },
+    { name: '7awli', emoji: '🍈' },
+    { name: 'khrof', emoji: '🍇' },
+    { name: 'me3za', emoji: '🍈' },
+    { name: 'jmel', emoji: '🍇' },
+    { name: 'fil', emoji: '🍇' },
+    { name: 'far', emoji: '🍇' },
+    { name: 'Tobba', emoji: '🍈' },
+    { name: '9nia', emoji: '🍇' },
+    { name: 'doub', emoji: '🍇' },
+    { name: 'kowala', emoji: '🍈' },
+    { name: 'panda', emoji: '🍇' },
+    { name: 'djaja', emoji: '🍈' },
+  ];
+}
